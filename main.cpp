61ef8f8634ab95cf7ef556ac88869f977e989a93
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-11 18:39:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include ...
+using namespace std;
 
-int main(){
-  
+int main()
+{
+int input;
+cin >> input;
+switch ( input )
+{
+case 1: 
+  break;
+case 2:
+  break;
+case 3:
+  break;
+case 4:
+  break;
 }
+
+}
+
+
+
