580799f9f99f080e19f3c0b1bca84d133444c6d3
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-12 19:14:40 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -6,7 +6,7 @@ int hospital(int super_powers, int health_points)
   string health_power[10] = { "First-aid kit", "endorphins shot", "glucose-D", "Vitamin-C tablets", "oxygen pack"
                              "ketamine shot", "epinephrin shot", "scapel", "syringe", "surgical drill machine"  }; 
   int hp_sp_increase[10] =  {50, 32, 28, 15, 30, 20, 30, 40, 14, 54};
-  int random_option;
+  int random_option; //randomly choosing 3 for the given options and presenting it to user
   random_option1 = rand() % 10;
   random_option2 = rand() % 10;
   random_option3 = rand() % 10;
@@ -22,8 +22,50 @@ int hospital(int super_powers, int health_points)
   else 
   {
     switch (input)
-      case 1:
-      cout << "You have chosen 
+      case 1: //if first one is chosen then the health/superpower is increased accordingly.
+      cout << "You have chosen the first option." << endl;
+      cout << "You get " << health_power[random_option1] << endl;
+      if (random_option1 < 5)
+      {
+        health_points += hp_sp_increase[random_option1];
+        cout << "Your health is increased to " << health_points << endl; 
+      }
+      else
+      {
+        super_powers += hp_sp_increase[random_option1];
+        cout << "Your superpowers have increased to " << super_powers << endl;
+      }
+      break;
+    
+      case 2:
+      cout << "You have chosen the first option." << endl;
+      cout << "You get " << health_power[random_option2] << endl;
+      if (random_option2 < 5)
+      {
+        health_points += hp_sp_increase[random_option2];
+        cout << "Your health is increased to " << health_points << endl; 
+      }
+      else
+      {
+        super_powers += hp_sp_increase[random_option2];
+        cout << "Your superpowers have increased to " << super_powers << endl;
+      }
+      break;
+    
+      case 3:
+      cout << "You have chosen the first option." << endl;
+      cout << "You get " << health_power[random_option3] << endl;
+      if (random_option3 < 5)
+      {
+        health_points += hp_sp_increase[random_option3];
+        cout << "Your health is increased to " << health_points << endl; 
+      }
+      else
+      {
+        super_powers += hp_sp_increase[random_option3];
+        cout << "Your superpowers have increased to " << super_powers << endl;
+      }
+      break;
   }
   
   
