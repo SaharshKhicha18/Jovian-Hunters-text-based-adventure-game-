3b26f3d4b50e1e144aac1c02d5e6b52475ffe19d
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-12 19:02:34 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -3,11 +3,29 @@ using namespace std;
 
 int hospital(int super_powers, int health_points)
 {
-  char health[10] = { 'healthkit', 'endorphins',  //add more items that increase health
-  char superpower[10] = { 'ketamineshop', 'epinephrinshot', 'scapel', 'syringe', } // add more 
-  int hp_increase[10] = {50, 32} //assignment of numeric health and superpower value
-  int sp_increase[10] = {60, 40}
-  for (int i = 0; i < 2; //size of the array 
-  int random_option = rand() % 4;
+  string health_power[10] = { "First-aid kit", "endorphins shot", "glucose-D", "Vitamin-C tablets", "oxygen pack"
+                             "ketamine shot", "epinephrin shot", "scapel", "syringe", "surgical drill machine"  }; 
+  int hp_sp_increase[10] =  {50, 32, 28, 15, 30, 20, 30, 40, 14, 54};
+  int random_option;
+  random_option1 = rand() % 10;
+  random_option2 = rand() % 10;
+  random_option3 = rand() % 10;
+  cout << "Press " << 1 << " for choosing " health_power[random_option1];
+  cout << "Press " << 2 << " for choosing " health_power[random_option2];
+  cout << "Press " << 3 << " for choosing " health_power[random_option3];
+  int input;
+  cin >> input;
+  if (input > 3)
+  {
+    cout << "invalid input" << endl;
+  }
+  else 
+  {
+    switch (input)
+      case 1:
+      cout << "You have chosen 
+  }
+  
+  
   
 }
