ce8d4718702924da9477e464d2e17a1383a2e52d
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-12 17:43:27 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -1,2 +1,8 @@
 #include <iostream>
 
+int hospital(int super_powers, int health_points)
+{
+  char health_superpowers[10] = { 'healthkit', 'endorphins', 'epinephrinshot', 'ketamineshot'}
+  int hp_increase[10] = {50, 32}
+  int sp_increase[10] = {60, 40}
+}
