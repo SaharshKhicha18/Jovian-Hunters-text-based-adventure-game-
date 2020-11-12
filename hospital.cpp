4d4caa30b3f0ba07e6ef3ffc1ebe7a04aec117b8
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-12 18:20:01 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int hospital(int super_powers, int health_points)
 {
-  char health_superpowers[10] = { 'healthkit', 'endorphins', 'epinephrinshot', 'ketamineshot'}
-  int hp_increase[10] = {50, 32}
+  char health[10] = { 'healthkit', 'endorphins',  //add more items that increase health
+  char superpower[10] = { 'ketamineshop', 'epinephrinshot', 'scapel', 'syringe', } // add more 
+  int hp_increase[10] = {50, 32} //assignment of numeric health and superpower value
   int sp_increase[10] = {60, 40}
+  for (int i = 0; i < 2; //size of the array 
+  int random_option = rand() % 4;
+  
 }
