e11ae9dbbdffc1883aa43d42a450b2cb255dff76
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-13 17:47:30 +0530

    Update gamecharacters.cpp

--- gamecharacters.cpp
+++ gamecharacters.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 gamecharacters(int hp, int ap){
+  initialhp = hp;
+  int choose;
+  string name;
+  cin >> choose;
+  while (choose <3 && choose >0){
+    if (choose == 1){
+      cout<< "Please input the name: ";
+      cin << name;
+      
+    
