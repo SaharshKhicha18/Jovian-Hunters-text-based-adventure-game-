f41a215d431f3a41ab3d36ff3abe5144d78858d5
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-17 17:12:17 +0530

    Update and rename Monsters.cpp to Monsters.h

--- Monsters.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-struct monsters{
-  int hp, ap;
-}  
---
+++ Monsters.h
@@ -0,0 +1,10 @@
+#include <string>
+#ifndef monsters_h
+#define monsters_h
+using namespace std;
+
+struct monsters{
+  Monster(int hp, int ap, string name);
+};  
+
+#endif
