32c4242f943dac6e3bae1141be11439e6ff298ab
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-18 14:44:21 +0800

    Update globalvar.h

--- globalvar.h
+++ globalvar.h
@@ -9,9 +9,11 @@ std::string level1names[] = {"Red Bull", "Fire Starter", "Gyros Platter"};
 int level2m[][2] = {{60, 12}, {63, 19}, {55, 21}};
 std::string level2names[] = {"Devil-Eye Joe", "Lonely Island", "Rasta-guana"};
 
+//level 3 monsters
 int level3m[][2] = {{70, 34}, {65, 29}, {63, 24}};
 std::string level3names[] = {"Boom Chick-a", "Hypno Toad", "Rocksteady"};
 
+//level 4 monsters
 int level4m[][2] = {{70, 34}, {65, 29}, {63, 24}};
 std::string level4names[] = {"Blue Eyes White Dragon", "Derpygama", "Squirrel Nut Zipper"};
 
