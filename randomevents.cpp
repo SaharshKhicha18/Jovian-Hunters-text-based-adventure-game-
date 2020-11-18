635ab6fff41bfc300d48aefbdcf1c989a14b53f5
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-18 13:17:47 +0530

    Update randomevents.cpp

--- randomevents.cpp
+++ randomevents.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int eventhap = 1; 
+int randnum = rand(15);
+switch (randomnum){
+  case 0 && 1 && 2 && 3 && 4 && 5:
+    #monsters will appear
+   
