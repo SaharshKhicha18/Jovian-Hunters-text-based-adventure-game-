1c59b8350300107f44cc813a61daee027ba5fd64
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-18 15:16:58 +0530

    Update randomevents.cpp

--- randomevents.cpp
+++ randomevents.cpp
@@ -2,8 +2,18 @@
 #include <cstdlib>
 using namespace std;
 int eventhap = 1; 
-int randnum = rand(15);
+int randnum = rand(25);
+while loop{
 switch (randomnum){
   case 0 && 1 && 2 && 3 && 4 && 5:
     #monsters will appear
-   
+  case 6 && 7:
+    #hospital will appear
+  case 8 && 9:
+    #ammunation will appear
+  case 10:
+    #monsters will appear
+  default:
+    #keep walking
+    #steps towards the kohinoor will increase by 1.
+    #after 10-12 steps level change?
