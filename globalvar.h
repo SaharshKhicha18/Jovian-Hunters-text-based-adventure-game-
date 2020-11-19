7ad559faf103e363a7006b804e66703210fc1399
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-19 16:06:37 +0530

    Update globalvar.h

--- globalvar.h
+++ globalvar.h
@@ -1,7 +1,15 @@
 #ifndef globalvar_h
 #define globalvar_h
 
-//level 1 monsters 
+//All necessary variables
+int charhp;
+int charap;
+int foehp;
+string charname;
+int gamepoints_at_end;
+int gamepoints;
+
+//All monsters 
 int monstersarray[][11] = {{40, 5}, {45, 7}, {43, 4},{60, 12}, {63, 19}, {55, 21},{70, 34}, {65, 29}, {63, 24},{70, 34}, {65, 29}, {63, 24}};
 std::string monsternames[] = {"Red Bull", "Fire Starter", "Gyros Platter","Devil-Eye Joe", "Lonely Island", "Rasta-guana","Boom Chick-a", "Hypno Toad", "Rocksteady","Blue Eyes White Dragon", "Derpygama", "Squirrel Nut Zipper"};
 
