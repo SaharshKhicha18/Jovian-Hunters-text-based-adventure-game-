e4c084bdbd7fc5c250590614740da0f575337495
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-19 14:59:13 +0530

    Update gamerundraft.cpp

--- gamerundraft.cpp
+++ gamerundraft.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include "events.h"
+#include "gamecharacters.h"
 
+int main(){
+  int charhp = 100;
+  int charap = 30;
+  int foehp;
+  string charname = "";
+  
+  int gamepoints_at_end = 80;
+  gamepoints = 0;
+  
+  //Introduction of the game
+  cout << "Welcome to the Jovian Hunters! An adventurous journey."<<endl;
+  cout << "You are a warrier who must save the precious jewel \"KOHINOOR DIAMOND\" from the monsters who invaded the city." << endl;
+  cout << "You will have to fight and kill 8 monsters to get to the jewel. Some are strong and others are weak" << endl;
+  cout << "Several places in the city hospitals, ammunition shop etc. where you can revive your health points as well as attack power using different weapons" <<endl;
+  
+  cout << "Choose you character name. You can choose you own name or choose the options provided to you by Jovian Hunters!" << endl;
+  gamecharacters( name);
+  
+  string line;
+  ifstream introduction ();
+  while(getline(introduction, line)){
+    cout<< line <<endl;
+  }
+  while (charhp >0 && gamepoints_at_end >0){
+    randomevents();
+    if (
+}
+
+
+    
