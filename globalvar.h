35d0708eddba606360814c97b1d507d012485c3b
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-19 11:36:08 +0530

    Update globalvar.h

--- globalvar.h
+++ globalvar.h
@@ -2,21 +2,10 @@
 #define globalvar_h
 
 //level 1 monsters 
-int level1m[][2] = {{40, 5}, {45, 7}, {43, 4}};
-std::string level1names[] = {"Red Bull", "Fire Starter", "Gyros Platter"};
+int monstersarray[][11] = {{40, 5}, {45, 7}, {43, 4},{60, 12}, {63, 19}, {55, 21},{70, 34}, {65, 29}, {63, 24},{70, 34}, {65, 29}, {63, 24}};
+std::string monsternames[] = {"Red Bull", "Fire Starter", "Gyros Platter","Devil-Eye Joe", "Lonely Island", "Rasta-guana","Boom Chick-a", "Hypno Toad", "Rocksteady","Blue Eyes White Dragon", "Derpygama", "Squirrel Nut Zipper"};
 
-//level 2 monsters
-int level2m[][2] = {{60, 12}, {63, 19}, {55, 21}};
-std::string level2names[] = {"Devil-Eye Joe", "Lonely Island", "Rasta-guana"};
-
-//level 3 monsters
-int level3m[][2] = {{70, 34}, {65, 29}, {63, 24}};
-std::string level3names[] = {"Boom Chick-a", "Hypno Toad", "Rocksteady"};
-
-//level 4 monsters
-int level4m[][2] = {{70, 34}, {65, 29}, {63, 24}};
-std::string level4names[] = {"Blue Eyes White Dragon", "Derpygama", "Squirrel Nut Zipper"};
-
+int gamepoints = 0;
 //step counter
 #endif 
 
