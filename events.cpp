0a1694d76ade228a8213ebfb43da91d0e8dcd4b1
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-19 12:38:59 +0530

    Update events.cpp

--- events.cpp
+++ events.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include "events.h"
+#include "globalvar.h"
 #include <map>
 using namespace std;
 
-void war( charhp, map<string, int> charap, foehp, forap){
+void war( charhp, charap, map<string, int> inventory, foehp, forap, string monster){
   while (charhp >0 && foehp >0){
     //player's turn
-    map<string, int>::iterator itr;
-    int choosein, i == 1; 
-    for(itr = charap.begin(); itr != charap.end(); itr++){
+    int choosein, i = 1, damage = 0; 
+    if (inventory.size == 0){
+      cout << "Your inventory is empty" << endl;
+    }
+    if (inventory.size = 1){
+      map<string, int>::iterator itr;
+      for(itr = inventory.begin(); itr != inventory.end(); itr++){
+      cout << i << ") " << "You only have " << (*itr).first << "with damage of " << (*itr).second << "." <<endl;
+      }
+      cout << "Input 1 to pick the weapon" <<endl;
+      cout << "Input 0 if you do not want to use any weapon" <<endl;
+      cout << "Input a number?(1 or 0)";
+      cin >> choosein;
+      bool flag = true;
+      while (flag == true){
+        if (choosein <=1 && choosein >=0){
+          if (choosein == 1){
+            cout << "You are attacking the "<< monster << " with a " << " << ; //have to figure how to add key here(monster name)
+            damage = inventory[key];
+            foehp -= damage; 
+          }
+          if (choosein == 0){
+            cout << "You attack the monster with you hands" << endl;
+            damage = charap;
+            foehp -= damage;
+          }
+        }
+        else{
+          cout << "Oops! No such option in the inventory. Choose again: ";
+          cin << choosein;
+        }
+    }
+    if (inventory.size <=3 && inventory.size > 1){
+      i = 1;
+      map<string, int>::iterator itr;
+      for(itr = inventory.begin(); itr != inventory.end(); itr++){
       cout << i << ") " << "You have " << (*itr).first << "with damage of " << (*itr).second << "." <<endl;
       i++;
+      }
+      cout << "Choose one option number from above?";
     }
-    cout << "Choose one from above. 1, 2 or 3?";
-    cout << "Input 0 if you do not want to use any weapon";
     cin >> choosein;
     if (choosein == 1){
       
