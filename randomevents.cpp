a1f3c0e0f2757b7fc447b70f010b481d9b939945
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-19 15:55:15 +0530

    Update randomevents.cpp

--- randomevents.cpp
+++ randomevents.cpp
@@ -7,13 +7,28 @@ while loop{
 switch (randomnum){
   case 0 && 1 && 2 && 3 && 4 && 5:
     #monsters will appear
+    cout << "Beware! There is a monster ahead!" << endl;
+    cout << "It is a " << monster << endl;
+    war(charhp, charap, map<string, int> inventory, foehp, forap, string monster);
   case 6 && 7:
     #hospital will appear
+    cout << "There is a hospital ahead!" << endl;
+    cout << " A chance for you to revive your health points" << endl;
+    hospital(map<string, int> inventory, charhp);
   case 8 && 9:
     #ammunation will appear
+    cout << "There is an ammunition shop at the end of this road. You can collect weapons to put it in your inventory." <<endl;
+    ammunationshop(map<string, int> inventory);
   case 10:
     #pawn shop will appear
+    cout << "WOW! A pawn shop ahead. Can help you revive heath points and attack power." << endl;
+    pawnshop(map<string, int> inventory, charhp);
   default:
     #keep walking
-    #steps towards the kohinoor will increase by 1.
-    #after 10-12 steps level change?
+    eventhap = 0;
+    steps_before++;
+    gamepoints--;
+    break;
+}
+    
+    
