c810eca955ac6cda7d39d2839f7351b2cfce4274
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-19 12:36:54 +0800

    Update events.cpp

--- events.cpp
+++ events.cpp
@@ -8,11 +8,11 @@ void war( charhp, map<string, int> charap, foehp, forap){
     //player's turn
     map<string, int>::iterator itr;
     int choosein, i == 0; 
-    for(itr = charap.begin(); charap.end(); itr++){
-      cout << i << ") " << "You have " << (*itr).first << "with damage of " << (*itr).second << "."<<endl;
+    for(itr = charap.begin(); itr != charap.end(); itr++){
+      cout << i << ") " << "You have " << (*itr).first << "with damage of " << (*itr).second << "." <<endl;
     }
-    cout << "Choose one from above. 1, 2 and 3?";
-    cout << "Input 0 if you do not want any to use any weapon";
+    cout << "Choose one from above. 1, 2 or 3?";
+    cout << "Input 0 if you do not want to use any weapon";
     cin >> choosein;
     if (choosein == 1){
       
