05e5537c34d432934067cd4e74bb9b76d27b3ac8
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-20 20:34:51 +0530

    Update randomevents.cpp

--- randomevents.cpp
+++ randomevents.cpp
@@ -9,26 +9,33 @@ switch (randomnum){
     #monsters will appear
     cout << "Beware! There is a monster ahead!" << endl;
     cout << "It is a " << monster << endl;
-    if (
     war( charhp, charap, inventory_p, inventory_pvalue, monsternames, monsters_info);
+    if (charap>0){
+      gamepoints +=10;
+    }
+    else{
+      cout << "Game over" << endl;
+    }
+    break;
   case 6 && 7:
     #hospital will appear
     cout << "There is a hospital ahead!" << endl;
     cout << " A chance for you to revive your health points" << endl;
     hospital(inventory_p, inventory_pvalue, health_points);
+    break;
   case 8 && 9:
     #ammunation will appear
     cout << "There is an ammunition shop at the end of this road. You can collect weapons to put it in your inventory." <<endl;
     ammunationshop(inventory_p, inventory_pvalue);
+    break;
   case 10:
     #pawn shop will appear
     cout << "WOW! A pawn shop ahead. Can help you revive heath points and attack power." << endl;
     pawnshop(inventory_p, inventory_pvalue, health_points);
+    break;
   default:
     #keep walking
     eventhap = 0;
-    gamepoints++;
-    gamepoints_at_end--;
     break;
 }
     
