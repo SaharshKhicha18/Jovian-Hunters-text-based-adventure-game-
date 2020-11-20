eaf6c21683b1a2e276bb08a189e6d801d12a1b15
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-20 12:32:35 +0800

    Update war.cpp

--- war.cpp
+++ war.cpp
@@ -15,9 +15,6 @@ void war( charhp, charap, vector<string> inventory_p, vector<int> inventory_pval
     }
     if (inventory_p.size() = 1){
       cout << " 1 " << "You only have " << inventory_p[0] << "with damage of " << inventory_pvalue[0] << "." <<endl;
-      //vector<string>::iterator itr;
-      //for(itr = inventory_p.begin(); itr != inventory_p.end(); itr++){
-      //cout << i << ") " << "You only have " << (*itr).first << "with damage of " << (*itr).second << "." <<endl;
       }
       cout << "Input 1 to pick the weapon" <<endl;
       cout << "Input 0 if you do not want to use any weapon" <<endl;
@@ -56,35 +53,34 @@ void war( charhp, charap, vector<string> inventory_p, vector<int> inventory_pval
       cout << "Input 0 if you do not want to use any weapon" <<endl;
     }
     cin >> choosein;
-    if (choosein == 1){
-      bool flag = true;
-      while (flag == true){
-        if (choosein <=3 && choosein >=0){
-          if (choosein == 3){
-            cout << "You are attacking the "<< monster << " with a " << inventory_p[2] << endl;
-            damage = inventory_pvalue[2]; 
-            foehp -= damage; 
-          }
-          if (choosein == 2){
-            cout << "You are attacking the "<< monster << " with a " << inventory_p[1] << endl;
-            damage = inventory_pvalue[1];
-            foehp -= damage; 
-          }
-          if (choosein == 1){
-            cout << "You are attacking the "<< monster << " with a " << inventory_p[0] << endl;
-            damage = inventory_pvalue[0];
-            foehp -= damage; 
-          }
-          if (choosein == 0){
-            cout << "You attack the monster with you hands" << endl;
-            damage = charap;
-            foehp -= damage;
-          }
+    bool flag = true;
+    while (flag == true){
+      if (choosein <=3 && choosein >=0){
+        if (choosein == 3){
+          cout << "You are attacking the "<< monster << " with a " << inventory_p[2] << endl;
+          damage = inventory_pvalue[2]; 
+          foehp -= damage; 
         }
-        else{
-          cout << "Oops! No such option in the inventory. Choose again: ";
-          cin >> choosein;
+        if (choosein == 2){
+          cout << "You are attacking the "<< monster << " with a " << inventory_p[1] << endl;
+          damage = inventory_pvalue[1];
+          foehp -= damage; 
         }
+        if (choosein == 1){
+          cout << "You are attacking the "<< monster << " with a " << inventory_p[0] << endl;
+          damage = inventory_pvalue[0];
+          foehp -= damage; 
+        }
+        if (choosein == 0){
+          cout << "You attack the monster with you hands" << endl;
+          damage = charap;
+          foehp -= damage;
+        }
+      }
+      else{
+        cout << "Oops! No such option in the inventory. Choose again: ";
+        cin >> choosein;
+      }
       }
       
     //monster's turn
