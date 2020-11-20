a34ecb42d3a5953ad60b5c1d6e438990ec25fea1
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-20 21:18:17 +0530

    Delete generalchar.cpp

--- generalchar.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "generalchar.h"
-using namespace std;
-
-void character(int hp, int ap, string name){
-  Character.hitp = hp;
-  Character.attackp = ap;
-  Character.charname = name;
-};
