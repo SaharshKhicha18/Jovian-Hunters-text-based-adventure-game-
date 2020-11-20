68d2c38715503c084fab8a99bab61dedfbfc5c70
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-20 22:44:32 +0800

    Update ammunationshop.cpp

--- ammunationshop.cpp
+++ ammunationshop.cpp
@@ -1,52 +1,65 @@
 #include <iostream>
 #include <string>
-#include <map>
+#include <vector>
 using namespace std;
 
-void ammunationshop(map<string, int> attack_power)
+void ammunationshop(vector<string> &inventory_p, vector<int> &inventory_pvalue)
 {
-  string health_power[10] = {"Remington", "Mossberg", "Smith & Wesson", "", "Silver knife",
-                             "Tikka", "Springfield", "Browning", "Stoeger", "Franchi"};
-  int hp_sp_increase[10] =  {53, 20, 54, 45, 21, 14, 21, 25, 17, 65};
-  srand (time(0));
-  int random_option; //randomly choosing 3 for the given options and presenting it to user
-  int random_option1 = rand() % 10; //random number between 0 and 9.
-  int random_option2 = rand() % 10;
-  int random_option3 = rand() % 10;
-  cout << "Press " << 1 << " to pick " << health_power[random_option1] << endl;
-  cout << "Press " << 2 << " to pick " << health_power[random_option2] << endl;
-  cout << "Press " << 3 << " to pick " << health_power[random_option3] << endl;
-  int input;
-  cin >> input;
-  if (input > 3)
+  if (inventory_p.size() == 3)
   {
-    cout << "invalid input" << endl;
+    cout << "Opps! You inventory is full! Use what you got to hunt some monsters down first!" << endl;
   }
   else
   {
-    switch (input)
+    string health_power[10] = {"Ak-47", "Pan", "Copper Dagger", "Machine-gun", "Silver knife",
+                               "S98 shotgun", "Pistol", "Kar-98 Sniper", "Grenade", "SMG-9"};
+    int hp_sp_increase[10] =  {53, 20, 54, 45, 21, 14, 21, 25, 17, 65};
+    srand (time(0));
+    int random_option; //randomly choosing 3 for the given options and presenting it to user
+    int random_option1 = rand() % 10; //random number between 0 and 9.
+    int random_option2 = rand() % 10;
+    int random_option3 = rand() % 10;
+    cout << "Press " << 1 << " to pick " << health_power[random_option1] << endl;
+    cout << "Press " << 2 << " to pick " << health_power[random_option2] << endl;
+    cout << "Press " << 3 << " to pick " << health_power[random_option3] << endl;
+    int input;
+    cin >> input;
+    while (input > 3)
     {
-      case 1: //if first one is chosen then the health/superpower is increased accordingly.
-      cout << "You have chosen the first option." << endl;
-      cout << "You get " << health_power[random_option1] << " with attack power of " << hp_sp_increase[random_option1] << endl;
-      attack_power[health_power[random_option1]] = hp_sp_increase[random_option1];
-      cout << health_power[random_option1] << " is added to you inventory, you may use it during war with monsters " << endl;
-      break;
+      cout << "invalid input" << endl;
+      cin >> input;
+    }
+    if (input <= 3)
+    {
+      switch (input)
+      {
+        case 1: //if first one is chosen then the health/superpower is increased accordingly.
+        cout << "You have chosen the first option." << endl;
+        cout << "You get " << health_power[random_option1] << " with attack power of " << hp_sp_increase[random_option1] << endl;
+        inventory_p.push_back(health_power[random_option1]);
+        inventory_pvalue.push_back(hp_sp_increase[random_option1]);
+        cout << health_power[random_option1] << " is added to you inventory." << endl;
+        cout << "You may use it during war with monsters. " << endl;
+        break;
 
-      case 2:
-      cout << "You have chosen the second option." << endl;
-      cout << "You get " << health_power[random_option2] << " with attack power of " << hp_sp_increase[random_option2] << endl;
-      attack_power[health_power[random_option2]] = hp_sp_increase[random_option2];
-      cout << health_power[random_option2] << " is added to you inventory, you may use it during war with monsters " << endl;
-      break;
+        case 2:
+        cout << "You have chosen the second option." << endl;
+        cout << "You get " << health_power[random_option2] << " with attack power of " << hp_sp_increase[random_option2] << endl;
+        inventory_p.push_back(health_power[random_option2]);
+        inventory_pvalue.push_back(hp_sp_increase[random_option2]);
+        cout << health_power[random_option2] << " is added to you inventory." << endl;
+        cout << "You may use it during war with monsters. " << endl;
+        break;
 
-      case 3:
-      cout << "You have chosen the third option." << endl;
-      cout << "You get " << health_power[random_option3] << " with attack power of " << hp_sp_increase[random_option3] << endl;
-      attack_power[health_power[random_option3]] = hp_sp_increase[random_option3];
-      cout << health_power[random_option3] << " is added to you inventory, you may use it during war with monsters " << endl;
-      break;
+        case 3:
+        cout << "You have chosen the third option." << endl;
+        cout << "You get " << health_power[random_option3] << " with attack power of " << hp_sp_increase[random_option3] << endl;
+        inventory_p.push_back(health_power[random_option3]);
+        inventory_pvalue.push_back(hp_sp_increase[random_option3]);
+        cout << health_power[random_option3] << " is added to you inventory." << endl;
+        cout << "You may use it during war with monsters. " << endl;
+        break;
+      }
     }
   }
-
-}
+  }
