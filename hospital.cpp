9852df4fa37c9f8bbae162f0e0373f2c55333033
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-21 20:54:35 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -8,8 +8,10 @@ using namespace std;
 
 void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &health_points)
 {
+  //array with health (first 5) and power (6th to 10th item) increasing items. 
   string health_power[10] = {"First-aid_kit", "E-drink", "Glucose-D", "Vitamin-C_tablets", "Oxygen_pack",
                              "Ketamine_shot", "Epinephrin_shot", "Scapel", "Syringe", "Surgical_drill_machine"};
+  //array with the values of health and power
   int hp_sp_increase[10] =  {75, 32, 28, 15, 30, 20, 30, 40, 20, 54};
   srand (time(0));
   int random_option; //randomly choosing 3 for the given options and presenting it to user
@@ -17,7 +19,9 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
   int random_option2 = rand() % 10;
   int random_option3 = rand() % 10;
 
+  //adding on to health points or powers to inventory
   cout << "Input " << 1 << " to pick " << health_power[random_option1];
+  //if the random number is less than 5, it belongs to health
   if (random_option1 < 5){
     cout << ". This will revive your health points" << endl;
   }
@@ -41,6 +45,7 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
   int input;
   cout << "Choose from 1, 2 or 3 -> ";
   cin >> input;
+  //defining the invalid inputs
   while (input > 3 || input < 1)
   {
     cout << "Invalid input" << endl;
@@ -76,7 +81,7 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
         else
         {
           cout << "You get " << health_power[random_option1] << " with attack power of " << hp_sp_increase[random_option1] << endl;
-          inventory_p.push_back(health_power[random_option1]);
+          inventory_p.push_back(health_power[random_option1]); //adding to the inventory vector
           inventory_pvalue.push_back(hp_sp_increase[random_option1]);
           cout << health_power[random_option1] << " is added to you inventory." << endl;
           cout << "You may use it during war with monsters. " << endl;
@@ -88,6 +93,7 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
       cout << "You have chosen the second option." << endl;
       if (random_option2 < 5)
       {
+        //if health reaches max, which is defined to be 120, cannot increase further
         if (health_points >= 120)
         {
           cout << "Ooooo! Your health is full! Please come back when your health is low" << endl;
@@ -103,6 +109,7 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
       }
       else
       {
+        //limiting the max no. of items in inventory to be 3.
         if (inventory_p.size() == 3)
         {
           cout << "Opps! You inventory is full! Use what you got to hunt some monsters down first!" << endl;
