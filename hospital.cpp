3d7704a24996938f56d4cf62e06a988257990fb3
Dahartz99 <71881195+Dahartz99@users.noreply.github.com>
2020-11-21 21:16:38 +0800

    Update hospital.cpp

--- hospital.cpp
+++ hospital.cpp
@@ -14,16 +14,16 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
   //array with the values of health and power
   int hp_sp_increase[10] =  {75, 32, 28, 15, 30, 20, 30, 40, 20, 54};
   srand (time(0));
-  int random_option; //randomly choosing 3 for the given options and presenting it to user
+  //generating 3 random numbers 
   int random_option1 = rand() % 10; //random number between 0 and 9.
   int random_option2 = rand() % 10;
   int random_option3 = rand() % 10;
 
-  //adding on to health points or powers to inventory
+  //giving 3 random options from the hospital to choose 
   cout << "Input " << 1 << " to pick " << health_power[random_option1];
   //if the random number is less than 5, it belongs to health
   if (random_option1 < 5){
-    cout << ". This will revive your health points" << endl;
+    cout << ". This will revive your health points" << endl; //description of what the item does
   }
   else{
     cout << ". This will be loaded to your inventory for future monster attacks" << endl;
@@ -55,48 +55,50 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
   {
     switch (input)
     {
+      //adding on to health points or powers to inventory
       case 1: //if first one is chosen then the health/superpower is increased accordingly.
       cout << "You have chosen the first option." << endl;
       if (random_option1 < 5)
       {
         if (health_points >= 120)
         {
+          //if health reaches max, which is defined to be 120, cannot increase further
           cout << "Ooooo! Your health is full! Please come back when your health is low" << endl;
         }
         else
         {
           cout << "You get " << health_power[random_option1] << " with " << hp_sp_increase[random_option1] << " health points" << endl;
-          health_points += hp_sp_increase[random_option1];
+          health_points += hp_sp_increase[random_option1]; //adding the health value of the item to health directly
           if (health_points > 120)
             health_points = 120;
-          cout << "Your health is increased to " << health_points << endl;
+          cout << "Your health is increased to " << health_points << endl; //confirmation of the health increase after picking the health increasing item
         }
       }
       else
       {
         if (inventory_p.size() == 3)
         {
-          cout << "Opps! You inventory is full! Use what you got to hunt some monsters down first!" << endl;
+          cout << "Opps! You inventory is full! Use what you got to hunt some monsters down first!" << endl; //limiting the max number of items in inventory to be 3.
         }
         else
         {
           cout << "You get " << health_power[random_option1] << " with attack power of " << hp_sp_increase[random_option1] << endl;
-          inventory_p.push_back(health_power[random_option1]); //adding items to the inventory vector
-          inventory_pvalue.push_back(hp_sp_increase[random_option1]);
-          cout << health_power[random_option1] << " is added to you inventory." << endl;
+          inventory_p.push_back(health_power[random_option1]); //adding items to the inventory 
+          inventory_pvalue.push_back(hp_sp_increase[random_option1]); // adding the health/power value to inventory
+          cout << health_power[random_option1] << " is added to you inventory." << endl; //confirmation of addition to inventory
           cout << endl;
         }
       }
       break;
 
-      case 2:
+      case 2: //same as above, but here when second option is chosen
       cout << "You have chosen the second option." << endl;
       if (random_option2 < 5)
       {
-        //if health reaches max, which is defined to be 120, cannot increase further
+        
         if (health_points >= 120)
         {
-          cout << "Ooooo! Your health is full! Please come back when your health is low" << endl;
+          cout << "Ooooo! Your health is full! Please come back when your health is low" << endl; 
         }
         else
         {
@@ -109,7 +111,6 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
       }
       else
       {
-        //limiting the max no. of items in inventory to be 3.
         if (inventory_p.size() == 3)
         {
           cout << "Opps! You inventory is full! Use what you got to hunt some monsters down first!" << endl;
@@ -125,7 +126,7 @@ void hospital(vector<string> &inventory_p, vector<int> &inventory_pvalue, int &h
       }
       break;
 
-      case 3:
+      case 3: //same as above, but here is when third option is chosen
       cout << "You have chosen the third option." << endl;
       if (random_option3 < 5)
       {
