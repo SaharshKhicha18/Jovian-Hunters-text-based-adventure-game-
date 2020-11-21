e0598edbfb3a0866f008a290b6dd04ba60804de8
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-21 17:56:06 +0530

    Delete menu.cpp

--- menu.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-cout << “Welcome to Jovian Hunters” << endl;
-cout << “Press 1 to start a new game” << endl;
-cout << “Press 2 to load game” << endl;
-cout << "Press 3 to see how to play" << end;
-cout << "Press 4 to exit the game" << endl;
