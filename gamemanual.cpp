91c855c255e633c9cc78310974d9c52d78ab14f6
SaharshKhicha18 <71884962+SaharshKhicha18@users.noreply.github.com>
2020-11-21 21:15:57 +0530

    Add files via upload

---
+++ gamemanual.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "Jovian.h" 
+
+void gamemanual()
+{
+  cout << endl;
+  cout << "Game Instructions" << endl;
+  cout << "*****************" << endl;
+  cout << "Welcome Aboard!!" << endl;
+  cout << "This is a text-based adventure game." << endl;
+  cout << "Player chooses a character first then the game starts." <<endl;
+  cout << "The game provides you with random situations including fighting the monsters."<<endl; 
+  cout << "Furthermore, there are different places such as hospitals, ammunationshop and pawnshops to revive heath points or attack powers during the game. " << endl;
+  cout << " Player is provided with an inventory where he/she can save upto 3 weapons, potions etc. to use while fighting monsters. " << endl;
+  cout << "Sole purpose of JOVIAN HUNTERS is to gain 80 gamepoints by killing 8 monsters to retrieve the \"KOHINOOR\" diamond." << endl;
+  cout << "Player is provided with 100 heath points and 17 attack power at the start." <<endl;
+  cout << "Killing the monsters and winning the game is quite difficult." << endl;
+  cout << "Best of luck!" << endl;
+  cout << "You are our last hope to save the precious KOHINOOR." << endl;
+  cout << endl;
+}
